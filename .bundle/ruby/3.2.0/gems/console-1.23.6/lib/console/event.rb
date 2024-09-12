468a872f985aac7e3876f3c1f1eacca2469ea002
AayushSharma-1 <94588354+AayushSharma-1@users.noreply.github.com>
2024-09-12 05:33:09 +0000

    removed_bundle

--- .bundle/ruby/3.2.0/gems/console-1.23.6/lib/console/event.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2019-2022, by Samuel Williams.
-
-require_relative 'event/spawn'
-require_relative 'event/failure'
-require_relative 'event/progress'
